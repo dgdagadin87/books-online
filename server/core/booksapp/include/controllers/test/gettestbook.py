aabf1186fac3978080d9ec28ad2fd7205655ee9d
Dmitry <dgdagadin87@mail.ru>
2018-08-17 17:58:37 +0300

    TEST: Controller was modified

--- server/core/booksapp/include/controllers/test/gettestbook.py
+++ server/core/booksapp/include/controllers/test/gettestbook.py
@@ -1,5 +1,5 @@
 import codecs
-from ...abstract.base_controller import BaseController
+from ...abstract.base_controller2 import BaseController
 
 
 def test_gettestbook_controller(request):
@@ -11,9 +11,6 @@ def test_gettestbook_controller(request):
 class GetTestBookController(BaseController):
 
     def run(self):
-        base_checks = self.base_checks()
-        #if base_checks is not None:
-        #    return self.response_to_client(base_checks)
 
         file = codecs.open('./booksapp/include/controllers/test/testbook.html', 'r', 'utf_8_sig')
         file_data = file.read()
