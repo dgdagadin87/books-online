0d2c0d84bc5d7ebacecc9dc48b7f0cdb14142783
Dmitry <dgdagadin87@mail.ru>
2018-08-25 13:03:01 +0300

    API: Add raw book controller was modified

--- server/core/booksapp/include/controllers/api/addrawbook.py
+++ server/core/booksapp/include/controllers/api/addrawbook.py
@@ -1,5 +1,6 @@
-from booksapp.models import Books, Books_2_users, Cached_books
-from ...abstract.base_controller import BaseController
+import threading
+from booksapp.models import Books, Books_2_users, Cached_books, Notifications
+from ...abstract.base_controller2 import BaseController, error_decorator
 from ...sites.ubooki.controller import UbookiCacheBook
 from ...miscutils.helpers import BooksHelpers
 
@@ -13,13 +14,15 @@ def api_addrawbook_controller(request):
 class AddRawBookController(BaseController):
 
     def run(self):
-        base_checks = self.base_checks()
-        if base_checks is not None:
-            return self.response_to_client(base_checks)
+
+        self._error_message = 'Произошла непредвиденная ошибка (добавить книгу)'
+
+        # Базовые проверки
+        self.base_checks()
 
         # 0) Если метод не POST
         if self._request.method != 'POST':
-            return self.standart_error()
+            return self.return_error()
 
         # 1)Данные из POST
         book_link = str(self._request.POST.get('bookLink'))
@@ -29,6 +32,18 @@ class AddRawBookController(BaseController):
 
         # 2) Берем или создаем книгу и возвращаем ее ИД
         try:
+            cached_book_object = Cached_books.objects.get(book_link=book_link)
+            return self._add_to_my_books({
+                'book_id': cached_book_object.cached_book_id,
+                'book_size': len(cached_book_object.book_content)
+            })
+        except Cached_books.DoesNotExist:
+            # Вызываем асинхронный поток
+            '''threading.Thread(target=self._add_callback, args=(book_link, book_author, book_genre, book_name)).start()
+            return self._return_data(None)'''
+        except Exception:
+            return self.return_error()
+        '''try:
             cached_book_object = Cached_books.objects.get(book_link=book_link)
             return self._add_to_my_books({
                 'book_id': cached_book_object.cached_book_id,
@@ -47,7 +62,7 @@ class AddRawBookController(BaseController):
             else:
                 return self._add_to_my_books(book_data)
         except Exception as e:
-            return self.standart_error(message=e)
+            return self.standart_error(message=e)'''
 
     def _add_to_my_books(self, cached_book_data):
 
@@ -60,9 +75,10 @@ class AddRawBookController(BaseController):
         except Books.DoesNotExist:
             book_exists = False
         except Exception as e:
-            return self.standart_error(message=e)
+            return self.return_error(message=e)
 
-        user_id = self._user_data['user'].user_id
+        user_data = self.get_user_data(True)
+        user_id = user_data.user_id
 
         if book_exists is True:
             book_id = book_object.book_id
@@ -91,29 +107,29 @@ class AddRawBookController(BaseController):
                 latest_id = latest_book.book_id
                 return self._add_book_to_user(latest_id, user_id)
             except Exception as e:
-                return self.standart_error(message=e)
+                return self.return_error(message=e)
 
     def _add_book_to_user(self, book_id, user_id):
-
         try:
             Books_2_users.objects.get(book_id_id=book_id, user_id_id=user_id)
-            return self._return_data()
+            return self._return_data(book_id)
         except Books_2_users.DoesNotExist:
             pass
         except Exception as e:
-            return self.standart_error(message=e)
+            return self.return_error(message=e)
 
         book_for_adding = Books_2_users(book_id_id=book_id, user_id_id=user_id)
 
         try:
             book_for_adding.save()
-            return self._return_data()
+            latest_book = Books.objects.latest('book_id')
+            latest_id = latest_book.book_id
+            return self._return_data(latest_id)
         except Exception as e:
-            return self.standart_error(message=e)
+            return self.return_error(message=e)
 
-    def _return_data(self, is_success=True, message=None):
+    def _return_data(self, book_id):
         return self.response_to_client({
-            'data': {},
-            'message': message,
-            'success': is_success
+            'bookId': book_id,
+            'hasInCache': False if book_id is None else True
         })
